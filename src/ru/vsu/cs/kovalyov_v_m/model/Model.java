8d566830c03fb02549dc8a0b280d64caa9fa0748
ytutq <qqqslv@gmail.com>
2024-12-18 16:03:57 +0300

    First commit with teacher's obj_reader

---
+++ src/ru/vsu/cs/kovalyov_v_m/model/Model.java
@@ -0,0 +1,27 @@
+package ru.vsu.cs.kovalyov_v_m.model;
+
+import ru.vsu.cs.kovalyov_v_m.math.Vector2f;
+import ru.vsu.cs.kovalyov_v_m.math.Vector3f;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Model {
+
+    public ArrayList<Vector3f> vertices = new ArrayList<Vector3f>();
+    public ArrayList<Vector2f> textureVertices = new ArrayList<Vector2f>();
+    public ArrayList<Vector3f> normals = new ArrayList<Vector3f>();
+    public ArrayList<Polygon> polygons = new ArrayList<Polygon>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Model model)) return false;
+        return Objects.equals(vertices, model.vertices) && Objects.equals(textureVertices, model.textureVertices) && Objects.equals(normals, model.normals) && Objects.equals(polygons, model.polygons);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(vertices, textureVertices, normals, polygons);
+    }
+}
---
+++ src/ru/vsu/cs/kovalyov_v_m/obj_reader/ObjReaderException.java
@@ -0,0 +1,7 @@
+package ru.vsu.cs.kovalyov_v_m.obj_reader;
+
+public class ObjReaderException extends RuntimeException {
+    public ObjReaderException(String errorMessage, int lineInd) {
+        super("Error parsing OBJ file on line: " + lineInd + ". " + errorMessage);
+    }
+}
